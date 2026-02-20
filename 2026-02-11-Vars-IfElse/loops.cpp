60b4918213cb4deacce4a7db87c7b11a0cd1c40d
iluvatar1 <woquendo@gmail.com>
2026-02-20 08:19:34 -0500

    Ejemplos de loops

---
+++ 2026-02-11-Vars-IfElse/loops.cpp
@@ -0,0 +1,12 @@
+#include <print>
+
+// learning the basics about loops
+int main(void)
+{
+    int ii = 20;
+    for(int ii = 0; ii < 10; ii++) {
+        std::println("{}", ii);
+    }
+    return 0;
+
+}
