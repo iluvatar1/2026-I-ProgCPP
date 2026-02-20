d5a754ba05a06629f2cf19576d8460f783836cd2
iluvatar1 <woquendo@gmail.com>
2026-02-20 08:19:54 -0500

    Funcion is prime

---
+++ 2026-02-20-Workshop/isprime.cpp
@@ -0,0 +1,35 @@
+#include <print> // println
+#include <iostream> // std::cin
+#include <cmath>
+
+bool isprime(long n);
+
+
+int main()
+{
+    long n = 10000000019;
+    //std::println("Dame un numero:");
+    //std::cin >> n;
+    std::println("{}", isprime(n));
+}
+
+
+bool isprime(long n)
+{ 
+    /*
+    un for entre ii = 1 hasta ii <= n
+    mirar si ii es divisor o no de n
+    */
+
+    if (n <= 1) {
+        return false;
+    }
+
+    for(long ii = 2; ii <= std::sqrt(n)+1; ii++){
+        if (n%ii == 0) {
+            return false;
+        }
+    }
+    return true;
+}
+
