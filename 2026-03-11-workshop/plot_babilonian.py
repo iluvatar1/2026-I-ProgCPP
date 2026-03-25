a0c83f023892d088ec7963e538ccdea7f7354e04
iluvatar1 <woquendo@gmail.com>
2026-03-25 09:00:31 -0500

    added some comments to plot python script

--- 2026-03-11-workshop/plot_babilonian.py
+++ 2026-03-11-workshop/plot_babilonian.py
@@ -1,8 +1,9 @@
+# include needed modules
 import numpy as np # para leer los datos y guardarlos en arreglos
 import matplotlib.pyplot as plt
 import sys
 
-# leer los datos
+# leer los datos: python plot.py datos.txt -> argv[1] : datos.txt
 ii, root, diff = np.genfromtxt(sys.argv[1], unpack=True)
 
 # dibujar y guardar a un pdf
@@ -17,4 +18,5 @@ ax.set_yscale("log")
 #ax.set_xscale("log")
 
 # Imprimir la figura
+plt.show()
 fig.savefig("plot.pdf")
