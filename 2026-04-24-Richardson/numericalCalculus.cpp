a4b14b973f56e54e672d4a15014d2506c5d4fa4a
iluvatar1 <woquendo@gmail.com>
2026-04-24 07:29:55 -0500

    Added and tested richardson with forward

---
+++ 2026-04-24-Richardson/numericalCalculus.cpp
@@ -0,0 +1,19 @@
+#include "numericalCalculus.h"
+
+double dforward(double x, double h, fptr f)
+{
+  return (f(x+h) - f(x))/(h);
+}
+
+double dcentral(double x, double h, fptr f)
+{
+  return (f(x+h) - f(x-h))/(2*h);
+}
+
+double richardson_forward(double x, double h, int p, fptr f) 
+{
+    double aux1 = dforward(x, h, f);
+    double aux2 = dforward(x, h/2, f);
+    double aux3 = std::pow(2.0, p);
+    return (aux3*aux2 - aux1)/(aux3 - 1);
+}
---
+++ 2026-04-24-Richardson/numericalCalculus.h
@@ -0,0 +1,10 @@
+#pragma once // resuelve el problema de incluirlo varias veces
+#include <functional>
+#include <cmath>
+
+using fptr = std::function<double(double)>;
+
+double dforward(double x, double h, fptr f);
+double dcentral(double x, double h, fptr f);
+
+double richardson_forward(double x, double h, int p, fptr f);
